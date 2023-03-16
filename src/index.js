9528b5d4a10603a1cb35992654d272cac63442fb
ravindra-kumar-eng <117750035+ravindra-kumar-eng@users.noreply.github.com>
2023-03-16 15:33:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+ReactDom.render(<React.StrictMode>
+                <App/>
+                </React.StrictMode>,document.getElementId('root')
+                );
+
 
 
